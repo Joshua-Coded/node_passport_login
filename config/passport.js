786a743cd87d6e765aac725800050e39c00528a3
Joshbam-joshwilly <Alanajoshua535@gmail.comgit config --global user.name Joshbam-joshwillygit config --global user.email Alanajoshua535@gmail.comgit config --global user.email Alanajoshua535@gmail.com>
2022-08-21 16:39:20 +0100

    authentication

---
+++ config/passport.js
@@ -0,0 +1,31 @@
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt  = require('bcryptjs');
+
+//LOAD USER MODEL
+const User = require('../models/User');
+
+module.exports = function(passport) {
+    passport.use(
+	new LocalStrategy({ usernameField: 'email' }, (email, password, done) =>{
+	    //Match user
+	    User.findOne({
+		email: email
+	    }).then(user => {
+		if(!user) {
+		    return done(null, false, { message: 'That email is not registered' });
+		}
+		//match password
+		bcrypt.compare(password, user.password, (err, isMatch) => {
+                    if(err) throw err;
+		    if(isMatch) {
+			return done(null, user);
+		    } else {
+			return done(null, false, {message: 'password is incorrect'});
+		    }
+		})
+	    })
+	})
+    );
+
+
+}
